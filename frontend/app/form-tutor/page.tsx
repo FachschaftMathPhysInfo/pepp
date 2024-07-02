0c7f7d907fb323e17297d611e26bea9cca018b05
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2024-07-02 12:12:23 +0200

    simplify html-tags

--- frontend/app/form-tutor/page.tsx
+++ frontend/app/form-tutor/page.tsx
@@ -3,39 +3,36 @@ import { Button } from "@/components/ui/button"
 import EventTable from "@/app/form-tutor/ui/table/table";
 import { Header } from "@/components/header"
 
-const inputDivCss = "w-96 my-3"
-const divCSS = "my-10 w-full h-full"
+const inputDivStyling = "w-96 my-3"
+const tableDivStyling = "my-10 w-full h-full"
 
 export default function Page() {
     return (
         <div className="w-full h-full">
             <Header></Header>
             <div className="w-fit mx-auto p-10">
-
                 <h1 className="font-bold text-2xl">Anmeldung Vorkurstutor:in</h1>
 
-                <div className={inputDivCss}>
-                    <Input placeholder="Vorname"></Input>
+                <div className={inputDivStyling}>
+                    <Input placeholder="Vorname"/>
                 </div>
 
-                <div className={inputDivCss}>
-                    <Input placeholder="Nachname"></Input>
+                <div className={inputDivStyling}>
+                    <Input placeholder="Nachname"/>
                 </div>
 
-                <div className={inputDivCss}>
-                    <Input type="email" placeholder="E-Mail"></Input>
+                <div className={inputDivStyling}>
+                    <Input type="email" placeholder="E-Mail" />
                 </div>
 
-                <div className={divCSS}>
-                    <EventTable></EventTable>
+                <div className={tableDivStyling}>
+                    <EventTable/>
                 </div>
 
-
-                <div className={inputDivCss}>
+                <div className={inputDivStyling}>
                     <Button>Submit</Button>
                 </div>
             </div>
-
         </div>
     )
 }
