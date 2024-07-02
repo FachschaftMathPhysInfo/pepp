ff269ad3ad1d9536664e34c0d2d6bcdaca08cc05
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2024-07-02 11:48:38 +0200

    fix smaller code smells

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,28 +1,23 @@
 import * as React from "react"
 import {LucideExternalLink} from "lucide-react";
 import Image from "next/image";
-import {Icon} from "lucide-react";
+import Link from "next/link";
 
-export interface HeaderProps
-    extends React.InputHTMLAttributes<HTMLInputElement> {
+// TODO: add actual links
+const Header = () => {
+    return (
+        <div className="w-full h-20 flex flex-row items-center justify-between px-12 pt-5 mb-5">
+            <Image src="/logo.png" alt="Logo der Fachschaft" width="200" height="20"></Image>
+            <span className="flex flex-row items-center justify-between w-1/6">
+                <Link href="/">Kontakt</Link>
+                <Link href="/" className="flex flex-row items-center">Homepage
+                    <LucideExternalLink className="ms-1" size="14"></LucideExternalLink>
+                </Link>
+            </span>
+        </div>
+    )
 }
 
-const Header = React.forwardRef<HTMLInputElement, HeaderProps>(
-    ({className, type, ...props}, ref) => {
-        return (
-            <div className="w-full h-20 flex flex-row items-center justify-between px-12 pt-5 mb-5">
-                <Image src="/logo.png" alt="Logo der Fachschaft" width="200" height="20"></Image>
-                <span className="flex flex-row items-center justify-between w-1/6">
-                        <a>Kontakt</a>
-                            <a className="flex flex-row items-center">Homepage
-                                <LucideExternalLink className="ms-1" size="14"></LucideExternalLink>
-                            </a>
-
-                    </span>
-            </div>
-        )
-    }
-)
 Header.displayName = "Header"
 
 export {Header}
