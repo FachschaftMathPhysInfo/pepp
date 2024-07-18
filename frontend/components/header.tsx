d15b7f8cf06185474655cebbff1874072163a5b1
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2024-07-18 15:21:32 +0200

    add responsive spacing to header.tsx

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -6,17 +6,20 @@ import Link from "next/link";
 // TODO: add actual links
 const Header = () => {
     return (
-        <div className="w-full h-20 flex flex-row items-center justify-between px-12 pt-5 mb-5">
-            <Image src="/logo.png" alt="Logo der Fachschaft" width="200" height="20"></Image>
-            <span className="flex flex-row items-center justify-between w-1/6">
-                <Link href="/">Kontakt</Link>
+        <div className="w-full h-20 flex flex-row items-center justify-between px-4 pt-5 mb-5 md:px-12">
+            <Image src="/logo.png" alt="Logo der Fachschaft" width="200" height="20" />
+            <span className="flex flex-row items-center justify-between w-auto space-x-0.5 sm:w-1/6 sm:space-x-0">
+                <Link href="/" className="mr-5">Kontakt</Link>
                 <Link href="/" className="flex flex-row items-center">Homepage
-                    <LucideExternalLink className="ms-1" size="14"></LucideExternalLink>
+                    <LucideExternalLink className="ml-1" size="14"/>
                 </Link>
             </span>
         </div>
-    )
+    );
 }
+
+export default Header;
+
 
 Header.displayName = "Header"
 
