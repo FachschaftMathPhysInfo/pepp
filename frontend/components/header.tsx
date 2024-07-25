c5998aff0bfd68361e64dbd8b682d1b26dabaccb
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2024-07-25 12:03:47 +0200

    fix header jumping

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -6,10 +6,10 @@ import Link from "next/link";
 // TODO: add actual links
 const Header = () => {
     return (
-        <div className="w-full h-20 flex flex-row items-center justify-between px-4 pt-5 mb-5 md:px-12">
+        <div className="w-full h-20 flex flex-row items-center justify-between mb-5 px-5">
             <Image src="/logo.png" alt="Logo der Fachschaft" width="200" height="20" />
-            <span className="flex flex-row items-center justify-between w-auto space-x-0.5 sm:w-1/6 sm:space-x-0">
-                <Link href="/" className="mr-5">Kontakt</Link>
+            <span className="flex flex-row items-center justify-between w-fit">
+                <Link href="/" className="mr-3">Kontakt</Link>
                 <Link href="/" className="flex flex-row items-center">Homepage
                     <LucideExternalLink className="ml-1" size="14"/>
                 </Link>
