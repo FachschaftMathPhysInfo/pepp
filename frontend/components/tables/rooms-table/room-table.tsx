e099ad03109bb90362860370002000efa769751e
calvalre <olga.sergeyeva@outlook.de>
2025-05-30 16:42:32 +0200

    Add Horizontal Scroll Area for Mobile View on all the tables

--- frontend/components/tables/rooms-table/room-table.tsx
+++ frontend/components/tables/rooms-table/room-table.tsx
@@ -5,27 +5,41 @@ import {
   getFilteredRowModel,
   getSortedRowModel,
   useReactTable,
-  VisibilityState
+  VisibilityState,
 } from "@tanstack/react-table";
-import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import React from "react";
-import {Input} from "@/components/ui/input";
-import {DataTablePagination} from "@/components/data-table-pagination";
-import {Building, Room} from "@/lib/gql/generated/graphql";
-import {RoomColumn} from "@/components/tables/rooms-table/room-columns";
-import {LocationDialogState} from "@/app/(settings)/admin/rooms/page";
-
+import { Input } from "@/components/ui/input";
+import { DataTablePagination } from "@/components/data-table-pagination";
+import { Building, Room } from "@/lib/gql/generated/graphql";
+import { RoomColumn } from "@/components/tables/rooms-table/room-columns";
+import { LocationDialogState } from "@/app/(settings)/admin/rooms/page";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 interface DataTableProps {
   data: Room[];
-  setDialogState: React.Dispatch<React.SetStateAction<LocationDialogState>>
+  setDialogState: React.Dispatch<React.SetStateAction<LocationDialogState>>;
   currentBuilding: Building;
 }
 
-export function RoomTable({data, setDialogState, currentBuilding}: DataTableProps) {
-  const columns = RoomColumn({ currentBuilding, setDialogState});
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
-  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
+export function RoomTable({
+  data,
+  setDialogState,
+  currentBuilding,
+}: DataTableProps) {
+  const columns = RoomColumn({ currentBuilding, setDialogState });
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
   const table = useReactTable({
     data,
     columns,
@@ -53,56 +67,62 @@ export function RoomTable({data, setDialogState, currentBuilding}: DataTableProp
         />
       </div>
       <div className="rounded-md border overflow-hidden">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead className={'text-left'} key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell className={'[&:not(:first-child)]:ml-8'} key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
+        <ScrollArea className="w-96 rounded-md border whitespace-nowrap">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead className={"text-left"} key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  Keine Ergebnisse.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell
+                        className={"[&:not(:first-child)]:ml-8"}
+                        key={cell.id}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    Keine Ergebnisse.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </div>
-      <DataTablePagination table={table} enableSelectionCounter={false}/>
+      <DataTablePagination table={table} enableSelectionCounter={false} />
     </div>
   );
 }
