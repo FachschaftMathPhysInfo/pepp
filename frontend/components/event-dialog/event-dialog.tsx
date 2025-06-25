cdc6b12e79a18196cc8ffdd39e7a15fa4065aa2e
Tim Grube <115377141+Plebysnacc@users.noreply.github.com>
2025-06-25 15:09:44 +0200

    Refactor Sign-In Dialog (#174)

--- frontend/components/event-dialog/event-dialog.tsx
+++ frontend/components/event-dialog/event-dialog.tsx
@@ -8,26 +8,17 @@ import {
   Role,
   TutorialToUserAssignment,
 } from "@/lib/gql/generated/graphql";
-import React, { useEffect, useState } from "react";
-import { Edit3 } from "lucide-react";
-import { Skeleton } from "@/components/ui/skeleton";
-import {
-  DialogHeader,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  Dialog,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Badge } from "@/components/ui/badge";
-import { SignInDialog } from "@/components/sign-in-dialog";
-import { useRefetch, useUser } from "../providers";
-import { getClient } from "@/lib/graphql";
-import { TutorialsTable } from "./tutorials-table";
-import { FullDateDescription } from "../full-date-description";
-import { defaultEvent, defaultTutorial, defaultUser } from "@/types/defaults";
-import { Button } from "../ui/button";
-import { EditEventView } from "./edit-event-view";
+import React, {useEffect, useState} from "react";
+import {Edit3} from "lucide-react";
+import {Skeleton} from "@/components/ui/skeleton";
+import {DialogContent, DialogDescription, DialogHeader, DialogTitle,} from "@/components/ui/dialog";
+import {AuthenticationDialog} from "@/components/authentication-dialog/authentication-dialog";
+import {useRefetch, useUser} from "../providers";
+import {getClient} from "@/lib/graphql";
+import {TutorialsTable} from "./tutorials-table";
+import {defaultEvent, defaultTutorial, defaultUser} from "@/types/defaults";
+import {Button} from "../ui/button";
+import {EditEventView} from "./edit-event-view";
 import EventDescription from "./event-description";
 
 interface EventDialogProps {
@@ -37,21 +28,18 @@ interface EventDialogProps {
 }
 
 export default function EventDialog({
-  id,
-  modify = false,
-  open,
-}: EventDialogProps) {
-  const { user } = useUser();
-  const { refetchKey } = useRefetch();
+                                      id,
+                                      modify = false,
+                                      open,
+                                    }: EventDialogProps) {
+  const {user} = useUser();
+  const {refetchKey} = useRefetch();
 
   const [event, setEvent] = useState<Event>();
   const [edit, setEdit] = useState(modify);
-  const [newAssignments, setNewAssignments] = useState<
-    TutorialToUserAssignment[]
-  >([]);
-  const [deleteAssignments, setDeleteAssignments] = useState<
-    TutorialToUserAssignment[]
-  >([]);
+  const [newAssignments, setNewAssignments] = useState<TutorialToUserAssignment[]>([]);
+  const [deleteAssignments, setDeleteAssignments] = useState<TutorialToUserAssignment[]>([]);
+  const [authenticationDialogOpen, setAuthenticationDialogOpen] = useState(false);
 
   useEffect(() => {
     if (!open && id) setEdit(false);
@@ -83,69 +71,75 @@ export default function EventDialog({
           tutorials: e.tutorials?.map((t) => ({
             ...defaultTutorial,
             ...t,
-            event: { ...defaultEvent, ID: id! },
-            tutors: t.tutors?.map((tu) => ({ ...defaultUser, ...tu })),
+            event: {...defaultEvent, ID: id!},
+            tutors: t.tutors?.map((tu) => ({...defaultUser, ...tu})),
           })),
         });
       }
     };
 
-    if (id) fetchEventData();
+    if (id) void fetchEventData();
   }, [id, open, refetchKey]);
 
   return edit ? (
-    <EditEventView event={event} />
+    <EditEventView event={event}/>
   ) : (
-    <DialogContent className="sm:min-w-[600px]">
-      {!event && id ? (
-        <div className="flex flex-col space-y-3">
-          <Skeleton className="h-5 w-[80px]" />
-          <Skeleton className="h-3 w-[200px]" />
-          <Skeleton className="h-[125px] w-full rounded-xl" />
-        </div>
-      ) : (
-        <div className="space-y-4">
-          <DialogHeader>
-            <DialogTitle>{event?.title}</DialogTitle>
-            <DialogDescription className="space-y-2">
-              <EventDescription event={event} />
-            </DialogDescription>
-          </DialogHeader>
+    <>
+      <DialogContent className="sm:min-w-[600px]">
+        {!event && id ? (
+          <div className="flex flex-col space-y-3">
+            <Skeleton className="h-5 w-[80px]"/>
+            <Skeleton className="h-3 w-[200px]"/>
+            <Skeleton className="h-[125px] w-full rounded-xl"/>
+          </div>
+        ) : (
+          <div className="space-y-4">
+            <DialogHeader>
+              <DialogTitle>{event?.title}</DialogTitle>
+              <DialogDescription className="space-y-2">
+                <EventDescription event={event}/>
+              </DialogDescription>
+            </DialogHeader>
 
-          {!user && event?.tutorials?.length && (
-            <div>
-              <span>Bitte </span>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <span className="cursor-pointer text-blue-500 hover:underline">
+            {!user && event?.tutorials?.length && (
+              <div>
+                <span>Bitte </span>
+                <span
+                  className="cursor-pointer text-blue-500 hover:underline"
+                  onClick={() => setAuthenticationDialogOpen(true)}
+                >
                     anmelden
                   </span>
-                </DialogTrigger>
-                <SignInDialog />
-              </Dialog>
-              <span>, um dich eintragen zu können.</span>
-            </div>
-          )}
-          <TutorialsTable
-            id={id!}
-            event={event!}
-            capacities={
-              event?.tutorials?.map((t) => t.room.capacity ?? 1) || []
-            }
-            edit={false}
-            newAssignments={newAssignments}
-            setNewAssignments={setNewAssignments}
-            deleteAssignments={deleteAssignments}
-            setDeleteAssignments={setDeleteAssignments}
-          />
-          {user?.role === Role.Admin && (
-            <Button variant="secondary" onClick={() => setEdit(true)}>
-              <Edit3 className="h-4 w-4" />
-              Bearbeiten
-            </Button>
-          )}
-        </div>
-      )}
-    </DialogContent>
+                <span>, um dich eintragen zu können.</span>
+              </div>
+            )}
+            <TutorialsTable
+              id={id!}
+              event={event!}
+              capacities={
+                event?.tutorials?.map((t) => t.room.capacity ?? 1) || []
+              }
+              edit={false}
+              newAssignments={newAssignments}
+              setNewAssignments={setNewAssignments}
+              deleteAssignments={deleteAssignments}
+              setDeleteAssignments={setDeleteAssignments}
+            />
+            {user?.role === Role.Admin && (
+              <Button variant="secondary" onClick={() => setEdit(true)}>
+                <Edit3 className="h-4 w-4"/>
+                Bearbeiten
+              </Button>
+            )}
+          </div>
+        )}
+      </DialogContent>
+
+      <AuthenticationDialog
+        open={authenticationDialogOpen}
+        closeDialog={() => setAuthenticationDialogOpen(false)}
+      />
+    </>
+
   );
 }
