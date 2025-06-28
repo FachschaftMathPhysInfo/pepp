27e47f7aab918fd08f2fb01848476d3b788e1dfa
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-06-28 14:18:59 +0200

    fix: build error

--- frontend/components/tutorial-detail-page/students-columns.tsx
+++ frontend/components/tutorial-detail-page/students-columns.tsx
@@ -5,7 +5,7 @@ import { User } from "@/lib/gql/generated/graphql";
 import { ColumnDef } from "@tanstack/react-table";
 import { SquareMinus } from "lucide-react";
 import React from "react";
-import { StudentTableDialogState } from "@/app/(settings)/profile/tutorials/[tutorial]/tutorial-page";
+import { StudentTableDialogState } from "./tutorial-page";
 
 export default function StudentsColumns(
   setDialog: React.Dispatch<React.SetStateAction<StudentTableDialogState>>
