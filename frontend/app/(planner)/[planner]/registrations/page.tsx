ceef0c2c1c461dc6b11835295e600aa34ab153ca
calvalre <olga.sergeyeva@outlook.de>
2025-06-28 12:20:25 +0200

    Add component for registrations view for planner events and add page for the registrations in the corresponding planner event

--- frontend/app/(planner)/[planner]/registrations/page.tsx
+++ frontend/app/(planner)/[planner]/registrations/page.tsx
@@ -1,16 +1,21 @@
 import {ManagementPageHeader} from "@/components/management-page-header";
 import {BookCheck} from "lucide-react";
 import {RegistrationsView} from "@/components/registrations-view";
+import {extractId} from "@/lib/utils";
 
-export default async function RegistrationsLayout() {
+export default async function RegistrationsLayout({params}: { params: Promise<{ planner: string }> }) {
+    const {planner} = await params;
+    const umbrellaId = extractId(planner);
     return (
         <div className="space-y-6">
+
             <ManagementPageHeader
                 iconNode={<BookCheck/>}
                 title={"Meine Anmeldungen"}
                 description={"Du bist zu folgenden Veranstaltungen angemeldet."}
             />
             <RegistrationsView
+                umbrellaID={umbrellaId}
             />
         </div>
     );
