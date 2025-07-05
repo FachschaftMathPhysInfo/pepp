c550ca9fc1558a0c808c0444291240b88b64a6c4
Daniel Heidemann <mail@dheidemann.de>
2025-07-05 20:16:31 +0200

    minor bugs in password reset

--- frontend/app/(settings)/profile/password-form.tsx
+++ frontend/app/(settings)/profile/password-form.tsx
@@ -1,53 +1,67 @@
-"use client"
+"use client";
 
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
-import {z} from "zod";
-import {Button} from "@/components/ui/button";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import React, {useState} from "react";
-import {Save} from "lucide-react";
-import {toast} from "sonner";
-import {useUser} from "@/components/providers";
-import {getClient} from "@/lib/graphql";
-import {LoginDocument, LoginQuery, UpdateUserDocument, UpdateUserMutation} from "@/lib/gql/generated/graphql";
-
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import React, { useState } from "react";
+import { Save } from "lucide-react";
+import { toast } from "sonner";
+import { useUser } from "@/components/providers";
+import { getClient } from "@/lib/graphql";
+import {
+  LoginDocument,
+  LoginQuery,
+  UpdateUserDocument,
+  UpdateUserMutation,
+} from "@/lib/gql/generated/graphql";
 
 export default function PasswordForm() {
-  const passwordFormSchema = z.object({
-    currentPassword: z.string().nonempty(),
-    newPassword: z
-      .string()
-      .min(8, {message: "Muss mindestens 8 Zeichen lang sein."})
-      .regex(new RegExp(".*[A-Z].*"), {
-        message: "Muss einen Großbuchstaben enthalten.",
-      })
-      .regex(new RegExp(".*\\d.*"), {message: "Muss eine Nummer enthalten."})
-      .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"), {
-        message: "Muss ein Sonderzeichen enthalten.",
-      }),
-    confirmNewPassword: z.string(),
-  }).refine((data) => data.newPassword === data.confirmNewPassword, {
-    path: ["confirmNewPassword"],
-    message: "Passwörter stimmen nicht überein.",
-  });
+  const passwordFormSchema = z
+    .object({
+      currentPassword: z.string().nonempty(),
+      newPassword: z
+        .string()
+        .min(8, { message: "Muss mindestens 8 Zeichen lang sein." })
+        .regex(new RegExp(".*[A-Z].*"), {
+          message: "Muss einen Großbuchstaben enthalten.",
+        })
+        .regex(new RegExp(".*\\d.*"), {
+          message: "Muss eine Nummer enthalten.",
+        })
+        .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"), {
+          message: "Muss ein Sonderzeichen enthalten.",
+        }),
+      confirmNewPassword: z.string(),
+    })
+    .refine((data) => data.newPassword === data.confirmNewPassword, {
+      path: ["confirmNewPassword"],
+      message: "Passwörter stimmen nicht überein.",
+    });
   const form = useForm<z.infer<typeof passwordFormSchema>>({
     resolver: zodResolver(passwordFormSchema),
     defaultValues: {
       currentPassword: "",
       newPassword: "",
-      confirmNewPassword: ""
+      confirmNewPassword: "",
     },
   });
   const [hasTriedToSubmit, setHasTriedToSubmit] = useState(false);
-  const {user, sid} = useUser();
+  const { user, sid } = useUser();
 
   async function onValidSubmit(userData: z.infer<typeof passwordFormSchema>) {
     if (!user) {
-      toast.error('Ein Fehler ist aufgetreten')
-      console.error('User is undefined in change password action')
-      return
+      toast.error("Ein Fehler ist aufgetreten");
+      return;
     }
 
     const client = getClient(String(sid));
@@ -56,53 +70,58 @@ export default function PasswordForm() {
     try {
       await client.request<LoginQuery>(LoginDocument, {
         mail: user.mail,
-        password: userData.currentPassword
-      })
+        password: userData.currentPassword,
+      });
 
-      if(form.formState.errors.currentPassword) form.clearErrors('currentPassword')
+      if (form.formState.errors.currentPassword)
+        form.clearErrors("currentPassword");
     } catch {
-      form.setError('currentPassword', {
-        message: 'Das angegebene Passwort ist falsch'
-      })
-      return
+      form.setError("currentPassword", {
+        message: "Das angegebene Passwort ist falsch",
+      });
+      return;
     }
 
     const updateData = {
       mail: user.mail,
-      fn: user.mail,
-      sn: user.mail,
+      fn: user.fn,
+      sn: user.sn,
       confirmed: user.confirmed,
       role: user.role,
-      password: userData.newPassword
-    }
+      password: userData.newPassword,
+    };
 
     try {
-      await client.request<UpdateUserMutation>(UpdateUserDocument, {user: updateData, mail: user.mail})
-      toast.info('Dein Passwort wurde verändert')
-      console.log(`Changed password for ${user.mail}`)
-      setHasTriedToSubmit(false)
-      form.reset()
+      await client.request<UpdateUserMutation>(UpdateUserDocument, {
+        user: updateData,
+        id: user.ID,
+      });
+      toast.info("Dein Passwort wurde verändert");
+      setHasTriedToSubmit(false);
+      form.reset();
     } catch (error) {
-      toast.error('Ein Fehler beim updaten des Passworts ist aufgetreten')
-      console.error(error)
+      toast.error("Beim Ändern deines Passworts ist ein Fehler aufgetreten.");
     }
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onValidSubmit, () => setHasTriedToSubmit(true))}
-            className="space-y-4 w-full">
-
+      <form
+        onSubmit={form.handleSubmit(onValidSubmit, () =>
+          setHasTriedToSubmit(true)
+        )}
+        className="space-y-4 w-full"
+      >
         <FormField
           control={form.control}
           name="currentPassword"
-          render={({field}) => (
-            <FormItem className={'flex-grow'}>
+          render={({ field }) => (
+            <FormItem className={"flex-grow"}>
               <FormLabel>Aktuelles Passwort</FormLabel>
               <FormControl>
-                <Input placeholder={''} type={"password"} {...field}/>
+                <Input placeholder={""} type={"password"} {...field} />
               </FormControl>
-              <FormMessage/>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -110,16 +129,13 @@ export default function PasswordForm() {
         <FormField
           control={form.control}
           name="newPassword"
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
               <FormLabel>Neues Passwort</FormLabel>
               <FormControl>
-                <Input
-                  placeholder={''}
-                  type={"password"}
-                  {...field}/>
+                <Input placeholder={""} type={"password"} {...field} />
               </FormControl>
-              <FormMessage/>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -127,27 +143,23 @@ export default function PasswordForm() {
         <FormField
           control={form.control}
           name="confirmNewPassword"
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
               <FormLabel>Passwort bestätigen</FormLabel>
               <FormControl>
-                <Input
-                  placeholder={''}
-                  type={"password"}
-                  {...field}/>
+                <Input placeholder={""} type={"password"} {...field} />
               </FormControl>
-              <FormMessage/>
+              <FormMessage />
             </FormItem>
           )}
         />
 
-        <div className={'w-full flex justify-end items-center gap-x-12 mt-8'}>
-
+        <div className={"w-full flex justify-end items-center gap-x-12 mt-8"}>
           <Button
             disabled={!form.formState.isValid && hasTriedToSubmit}
             type="submit"
           >
-            <Save/>
+            <Save />
             Speichern
           </Button>
         </div>
