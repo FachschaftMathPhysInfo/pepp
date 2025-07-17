8f8cc1ba85daa18c47114c6df78dd26b46003185
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-07-17 12:00:41 +0200

    bring back create event button

--- frontend/app/(planner)/[planner]/edit-planner-section.tsx
+++ frontend/app/(planner)/[planner]/edit-planner-section.tsx
@@ -1,14 +1,17 @@
 import {Button} from "@/components/ui/button";
 import {Event,} from "@/lib/gql/generated/graphql";
-import {Edit2} from "lucide-react";
+import {Edit2, PlusCircle} from "lucide-react";
 import {useState} from "react";
 import {UmbrellaDialog} from "@/components/dialog/umbrellas/umbrella-dialog";
+import {Dialog} from "@/components/ui/dialog";
+import EventDialog from "@/components/dialog/events/event-dialog";
 
 interface EditPlannerSectionProps {
   umbrella: Event;
 }
 
-export default function EditPlannerSection({ umbrella }: EditPlannerSectionProps) {
+export default function EditPlannerSection({umbrella}: EditPlannerSectionProps) {
+  const [umbrellaDialogOpen, setUmbrellaDialogOpen] = useState(false);
   const [eventDialogOpen, setEventDialogOpen] = useState(false);
 
   if (!umbrella) return;
@@ -17,18 +20,31 @@ export default function EditPlannerSection({ umbrella }: EditPlannerSectionProps
     <>
       <div className="flex flex-row space-x-2">
         <h1 className="text-3xl font-semibold">{umbrella.title}</h1>
-        <Button variant={"ghost"} onClick={() => setEventDialogOpen(true)}>
+        <Button variant={"ghost"} onClick={() => setUmbrellaDialogOpen(true)}>
           <Edit2/>
+        </Button>
+        <Button variant={"secondary"} onClick={() => setEventDialogOpen(true)}>
+          <PlusCircle className={'inline mr-2'}/>
+          Event hinzufügen
         </Button>
       </div>
 
       <UmbrellaDialog
         umbrella={umbrella}
         umbrellas={[umbrella]}
-        isOpen={eventDialogOpen}
-        closeDialog={() => setEventDialogOpen(false)}
+        isOpen={umbrellaDialogOpen}
+        closeDialog={() => setUmbrellaDialogOpen(false)}
         createMode={false}
       />
+
+      <Dialog
+        open={eventDialogOpen}
+        onOpenChange={setEventDialogOpen}
+      >
+        <EventDialog
+          open={eventDialogOpen}
+          modify/>
+      </Dialog>
     </>
   );
 }
