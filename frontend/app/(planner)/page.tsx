d90dd06efb4ee9aca9033599937eaa2ee1ccb071
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-08-17 13:27:39 +0200

    refactor: indexpage loading

--- frontend/app/(planner)/page.tsx
+++ frontend/app/(planner)/page.tsx
@@ -1,10 +1,38 @@
-import {CalendarOff} from "lucide-react";
+"use client"
+
+import {CalendarOff, Loader2} from "lucide-react";
+import {useEffect, useState} from "react";
+import {getClient} from "@/lib/graphql";
+import {FutureEventsDocument, FutureEventsQuery} from "@/lib/gql/generated/graphql";
 
 export default function IndexPage() {
+  const [umbrellasInFutureExist, setUmbrellasInFutureExist] = useState(true);
+
+  useEffect(() => {
+    const fetchUmbrellasInFuture = async () => {
+      const client = getClient();
+      const response = await client.request<FutureEventsQuery>(FutureEventsDocument)
+      setUmbrellasInFutureExist(!!response.events);
+    }
+
+    void fetchUmbrellasInFuture();
+  }, [])
+
   return (
     <div className={'w-fill h-[60vh] flex flex-col gap-y-8 justify-center items-center text-center'}>
-      <CalendarOff size={100} className={'flex-shrink-0'}/>
-      Aktuell sind noch keine Veranstaltungen geplant.
+      {umbrellasInFutureExist ? (
+        <>
+          <Loader2 size={100} className={'animate-spin'} />
+          Lade Programme
+        </>
+      ) : (
+        <>
+          <CalendarOff size={100} className={'flex-shrink-0'}/>
+          Aktuell sind noch keine Veranstaltungen geplant.
+        </>
+      )}
+
+
     </div>
   )
 }
