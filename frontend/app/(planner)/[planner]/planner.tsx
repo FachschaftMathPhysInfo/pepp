93f5bb4511c2f9441576bbceae52f672ed634dcc
Daniel Heidemann <mail@dheidemann.de>
2025-09-24 17:00:56 +0200

    FR: Contact Info links on Homepage (#296)

--- frontend/app/(planner)/[planner]/planner.tsx
+++ frontend/app/(planner)/[planner]/planner.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { QRCodeSVG } from "qrcode.react";
 import {
   Event,
   Label,
@@ -7,35 +8,61 @@ import {
   PlannerEventsQuery,
   PlannerEventsQueryVariables,
   Role,
+  Setting,
+  SettingsDocument,
+  SettingsQuery,
   UmbrellaDetailDocument,
   UmbrellaDetailQuery,
 } from "@/lib/gql/generated/graphql";
-import {FacetedFilter} from "@/components/faceted-filter";
-import React, {useCallback, useEffect, useState} from "react";
-import {usePathname, useRouter, useSearchParams} from "next/navigation";
-import {getClient} from "@/lib/graphql";
-import {CopyTextArea} from "@/components/copy-text-area";
-import {useRefetch, useUser} from "@/components/providers";
-import {Alert, AlertAction, AlertDescription, AlertTitle,} from "@/components/ui/alert";
-import {CircleAlert, FunnelPlus, Loader2, MoveRight} from "lucide-react";
-import {defaultEvent} from "@/types/defaults";
+import { FacetedFilter } from "@/components/faceted-filter";
+import React, { useCallback, useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { getClient } from "@/lib/graphql";
+import { CopyTextArea } from "@/components/copy-text-area";
+import { useRefetch, useUser } from "@/components/providers";
+import {
+  Alert,
+  AlertAction,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert";
+import { CircleAlert, FunnelPlus, Loader2, MoveRight } from "lucide-react";
+import { defaultEvent } from "@/types/defaults";
 import EditPlannerSection from "./edit-planner-section";
-import {TooltipProvider} from "@/components/ui/tooltip";
-import {EventCalendar} from "@/components/event-calendar";
-import {createNewQueryString, getFiltersFromQuery, TOPICFILTER_QUERY_KEY, TYPEFILTER_QUERY_KEY} from "@/lib/query-urls";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { EventCalendar } from "@/components/event-calendar";
+import {
+  createNewQueryString,
+  getFiltersFromQuery,
+  TOPICFILTER_QUERY_KEY,
+  TYPEFILTER_QUERY_KEY,
+} from "@/lib/query-urls";
+import Link from "next/link";
+import {
+  DiscordIcon,
+  MatrixIcon,
+  WhatsappIcon,
+} from "@/components/social-icons";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { toast } from "sonner";
 
 interface PlannerPageProps {
   umbrellaID: number;
 }
 
-export function PlannerPage({umbrellaID}: PlannerPageProps) {
+export function PlannerPage({ umbrellaID }: PlannerPageProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
   const pathname = usePathname();
 
-  const {user} = useUser();
-  const {refetchKey} = useRefetch();
+  const { user } = useUser();
+  const { refetchKey } = useRefetch();
 
+  const [socials, setSocials] = useState<Setting[] | undefined>(undefined);
   const [events, setEvents] = useState<Event[]>([]);
   const [types, setTypes] = useState<Label[]>([]);
   const [topics, setTopics] = useState<Label[]>([]);
@@ -45,7 +72,8 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
   const [umbrellaLoading, setUmbrellaLoading] = useState<boolean>(true);
   const [isRestricted, setIsRestricted] = useState(false);
   const [umbrella, setUmbrella] = useState<Event>(defaultEvent);
-  const [hasInitializedFromParams, setHasInitializedFromParams] = useState<boolean>(false);
+  const [hasInitializedFromParams, setHasInitializedFromParams] =
+    useState<boolean>(false);
 
   const fetchUmbrellaData = useCallback(async () => {
     setUmbrellaLoading(true);
@@ -53,7 +81,7 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
 
     const umbrellaData = await client.request<UmbrellaDetailQuery>(
       UmbrellaDetailDocument,
-      {id: umbrellaID}
+      { id: umbrellaID }
     );
 
     if (umbrellaData) {
@@ -73,9 +101,9 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
   }, [umbrellaID]);
 
   useEffect(() => {
-    const fetchEventData = async () => {
-      const client = getClient();
+    const client = getClient();
 
+    const fetchEventData = async () => {
       const vars: PlannerEventsQueryVariables = {
         umbrellaID: umbrellaID ?? 0,
         topic: topicFilter.length ? topicFilter : undefined,
@@ -104,41 +132,60 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
   }, [topicFilter, typesFilter, umbrellaID, refetchKey]);
 
   useEffect(() => {
+    const client = getClient();
+    const fetchSocial = async () => {
+      try {
+        const settingsData = await client.request<SettingsQuery>(
+          SettingsDocument,
+          {
+            key: ["social-whatsapp", "social-discord", "social-matrix"],
+          }
+        );
+        setSocials(settingsData.settings);
+      } catch {
+        toast.error("Fehler beim Laden der Einstellungen.");
+      }
+    };
+
+    void fetchSocial();
+  }, []);
+
+  useEffect(() => {
     void fetchUmbrellaData();
   }, [refetchKey]);
 
   // Builds Query URL for sharing filters
   useEffect(() => {
     const topicFilterNames = topics
-      .filter(t => topicFilter.includes(t.ID))
-      .map(t => t.name)
+      .filter((t) => topicFilter.includes(t.ID))
+      .map((t) => t.name);
     const typesFilterNames = types
-      .filter(t => typesFilter.includes(t.ID))
-      .map(t => t.name)
+      .filter((t) => typesFilter.includes(t.ID))
+      .map((t) => t.name);
 
     router.push(
       pathname +
-      "?" +
-      createNewQueryString(TOPICFILTER_QUERY_KEY, topicFilterNames) +
-      (typesFilter.length && topicFilter.length ? "&" : "") +
-      createNewQueryString(TYPEFILTER_QUERY_KEY, typesFilterNames)
+        "?" +
+        createNewQueryString(TOPICFILTER_QUERY_KEY, topicFilterNames) +
+        (typesFilter.length && topicFilter.length ? "&" : "") +
+        createNewQueryString(TYPEFILTER_QUERY_KEY, typesFilterNames)
     );
   }, [topicFilter, typesFilter]);
 
   // Initializes filters from search params
   useEffect(() => {
     // Locks this after URL init
-    if(hasInitializedFromParams) return;
+    if (hasInitializedFromParams) return;
     // Awaits labels to load
-    if(!topics.length && !types.length) return;
+    if (!topics.length && !types.length) return;
 
-    const filterNames = getFiltersFromQuery(searchParams)
+    const filterNames = getFiltersFromQuery(searchParams);
     const typeFilters = types
-      .filter(t => filterNames.types.includes(t.name))
-      .map(t => t.ID)
+      .filter((t) => filterNames.types.includes(t.name))
+      .map((t) => t.ID);
     const topicFilters = topics
-      .filter(t => filterNames.topics.includes(t.name))
-      .map(t => t.ID)
+      .filter((t) => filterNames.topics.includes(t.name))
+      .map((t) => t.ID);
 
     setTypesFilter(typeFilters);
     setTopicFilter(topicFilters);
@@ -149,13 +196,12 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
     void fetchUmbrellaData();
   }, [umbrellaID]);
 
-
   useEffect(() => {
     setIcalPath(
       window.location.origin +
-      "/ical/?e=" +
-      umbrellaID +
-      (searchParams.size ? "&" + searchParams : "")
+        "/ical/?e=" +
+        umbrellaID +
+        (searchParams.size ? "&" + searchParams : "")
     );
   }, [searchParams]);
 
@@ -163,19 +209,30 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
     (a) => a.event.ID === umbrellaID
   );
 
-  const accepted: boolean = !!user?.applications?.find((a) => a.event.ID === umbrellaID)?.accepted;
+  const accepted: boolean = !!user?.applications?.find(
+    (a) => a.event.ID === umbrellaID
+  )?.accepted;
+
+  function getSocialValue(key: string) {
+    const s = socials?.find((s) => s.key === key);
+    return s ? s.value : "";
+  }
+
+  const wa = getSocialValue("social-whatsapp");
+  const matrix = getSocialValue("social-matrix");
+  const discord = getSocialValue("social-discord");
 
   return umbrellaLoading ? (
     <div className="flex flex-1 justify-center items-center text-center">
       <span className={"flex flex-col gap-y-2"}>
-        <Loader2 size={50} className="animate-spin text-gray-400"/>
+        <Loader2 size={50} className="animate-spin text-gray-400" />
       </span>
     </div>
   ) : (
     <TooltipProvider delayDuration={0}>
       {user?.role === Role.Admin && (
         <section className="mb-[20px] space-y-5">
-          <EditPlannerSection umbrella={umbrella}/>
+          <EditPlannerSection umbrella={umbrella} />
         </section>
       )}
 
@@ -183,18 +240,18 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
         <section className="flex items-stretch justify-between flex-wrap gap-4 mt-4">
           {(topics.length >= 2 || types.length >= 2) && (
             <div className="flex items-center gap-x-2 border p-2 rounded-lg">
-              <FunnelPlus size={20} className={'flex-shrink-0'}/>
+              <FunnelPlus size={20} className={"flex-shrink-0"} />
 
-              <span className={'flex flex-wrap items-center gap-2'}>
-               {topics.length >= 2 && (
-                 <FacetedFilter
-                   className={"h-full"}
-                   options={topics}
-                   filters={topicFilter}
-                   setFilter={setTopicFilter}
-                   title={"Studiengänge"}
-                 />
-               )}
+              <span className={"flex flex-wrap items-center gap-2"}>
+                {topics.length >= 2 && (
+                  <FacetedFilter
+                    className={"h-full"}
+                    options={topics}
+                    filters={topicFilter}
+                    setFilter={setTopicFilter}
+                    title={"Studiengänge"}
+                  />
+                )}
 
                 {types.length >= 2 && (
                   <FacetedFilter
@@ -209,7 +266,49 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
             </div>
           )}
 
-          <div className={'h-full'}><CopyTextArea label="ICS-Kalender" text={icalPath}/></div>
+          <div className="flex flex-row gap-x-2">
+            <CopyTextArea
+              className="w-40 h-full"
+              label="ICS-Kalender"
+              text={icalPath}
+            />
+            {(wa || matrix || discord) && (
+              <Popover>
+                <PopoverTrigger asChild>
+                  <div className="flex flex-col justify-between p-2 border rounded-lg">
+                    <div className="space-x-2 flex flex-row">
+                      {wa && <WhatsappIcon className="size-4" />}
+                      {discord && <DiscordIcon className="size-4" />}
+                    </div>
+                    {matrix && <MatrixIcon className="h-4" />}
+                  </div>
+                </PopoverTrigger>
+                <PopoverContent className="flex flex-row gap-x-4 w-fit">
+                  {wa && (
+                    <ShareArea
+                      icon={<WhatsappIcon className="size-4" />}
+                      label="Aktuelle WhatsApp Gruppe"
+                      link={wa}
+                    />
+                  )}
+                  {matrix && (
+                    <ShareArea
+                      icon={<MatrixIcon className="h-4 w-auto" />}
+                      label="Aktuelle Matrix Gruppe"
+                      link={matrix}
+                    />
+                  )}
+                  {discord && (
+                    <ShareArea
+                      icon={<DiscordIcon className="size-4" />}
+                      label="Unser Fachschafts Discord Server"
+                      link={discord}
+                    />
+                  )}
+                </PopoverContent>
+              </Popover>
+            )}
+          </div>
         </section>
       )}
 
@@ -220,7 +319,7 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
             onClick={() => router.push(`${pathname}/register`)}
             variant="warning"
           >
-            <CircleAlert className="size-4"/>
+            <CircleAlert className="size-4" />
             <AlertTitle className="font-bold">
               Registrierung erforderlich!
             </AlertTitle>
@@ -230,7 +329,7 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
               wir einschätzen können, ob du diesen Kurs benötigst.
             </AlertDescription>
             <AlertAction>
-              <MoveRight className="size-4"/>
+              <MoveRight className="size-4" />
             </AlertAction>
           </Alert>
         </section>
@@ -238,25 +337,45 @@ export function PlannerPage({umbrellaID}: PlannerPageProps) {
 
       {isRestricted && application && !accepted && (
         <section>
-          <Alert
-            className={"bg-destructive-foreground my-4"}
-            variant="warning"
-          >
-            <CircleAlert className="size-4"/>
+          <Alert className={"bg-destructive-foreground my-4"} variant="warning">
+            <CircleAlert className="size-4" />
             <AlertTitle className="font-bold">
               Registrierung eingegangen!
             </AlertTitle>
             <AlertDescription className="pr-8">
-              Du hast dich bereits für dieses Event registriert. Deine Registrierung ist eingegangen, jedoch haben wir
-              diese noch nicht bearbeitet.
+              Du hast dich bereits für dieses Event registriert. Deine
+              Registrierung ist eingegangen, jedoch haben wir diese noch nicht
+              bearbeitet.
             </AlertDescription>
           </Alert>
         </section>
       )}
 
       <section className="mt-5">
-        <EventCalendar events={events} initialView={"agenda"}/>
+        <EventCalendar events={events} initialView={"agenda"} />
       </section>
     </TooltipProvider>
+  );
+}
+
+interface ShareAreaProps {
+  label: string;
+  link: string;
+  icon: React.ReactNode;
+}
+
+function ShareArea({ label, link, icon }: ShareAreaProps) {
+  return (
+    <div className="items-center gap-y-2 w-[130px] flex flex-col">
+      {icon}
+      <p className="text-xs font-bold">{label}</p>
+      <Link
+        href={link}
+        className="text-blue-600 hover:underline truncate w-[130px]"
+      >
+        {link}
+      </Link>
+      <QRCodeSVG value={link} />
+    </div>
   );
 }
---
+++ frontend/app/(settings)/admin/social/page.tsx
@@ -0,0 +1,212 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Globe, House, Mail, Phone, Save } from "lucide-react";
+import {
+  ScalarType,
+  Setting,
+  SettingsDocument,
+  SettingsQuery,
+  UpsertSettingDocument,
+  UpsertSettingMutation,
+} from "@/lib/gql/generated/graphql";
+import { getClient } from "@/lib/graphql";
+import { useUser } from "@/components/providers";
+import React, { useEffect, useState } from "react";
+import { ManagementPageHeader } from "@/components/management-page-header";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  DiscordIcon,
+  GithubIcon,
+  InstagramIcon,
+  MatrixIcon,
+  WhatsappIcon,
+} from "@/components/social-icons";
+
+const FormSchema = z.object({
+  input: z.string(),
+});
+
+export default function IndexPage() {
+  const { sid } = useUser();
+  const [settings, setSettings] = useState<Setting[] | undefined>(undefined);
+  useEffect(() => {
+    if (!sid) return;
+    const client = getClient(sid!);
+    const fetchData = async () => {
+      try {
+        const settingsData = await client.request<SettingsQuery>(
+          SettingsDocument,
+          {
+            key: [
+              "social-telephone",
+              "social-mail",
+              "social-whatsapp",
+              "social-instagram",
+              "social-discord",
+              "social-matrix",
+              "social-homepage",
+              "social-github",
+            ],
+          }
+        );
+        setSettings(settingsData.settings);
+      } catch {
+        toast.error("Fehler beim Laden der Einstellungen.");
+      }
+    };
+
+    fetchData();
+  }, [sid]);
+
+  function getSettingValue(key: string) {
+    const s = settings?.find((s) => s.key === key);
+    return s ? s.value : "";
+  }
+
+  return (
+    settings && (
+      <section className="space-y-6">
+        <ManagementPageHeader
+          iconNode={<Globe />}
+          title={"Kontakt"}
+          description={
+            "Verwalte hier die Kontaktmöglichkeiten, welche über dem Header angezeigt werden. Leere Felder werden nicht angezeigt."
+          }
+        />
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-md">Klassisch</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <SocialSettingInput
+              settingKey="social-telephone"
+              settingValue={getSettingValue("social-telephone")}
+              icon={<Phone className="size-4" />}
+              placeholder="Telefonnummer"
+            />
+            <SocialSettingInput
+              settingKey="social-mail"
+              settingValue={getSettingValue("social-mail")}
+              icon={<Mail className="size-4" />}
+              placeholder="E-Mail Adresse"
+            />
+            <SocialSettingInput
+              settingKey="social-homepage"
+              settingValue={getSettingValue("social-homepage")}
+              icon={<House className="size-4" />}
+              placeholder="Homepage-Link"
+            />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-md">Social Media</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <SocialSettingInput
+              settingKey="social-whatsapp"
+              settingValue={getSettingValue("social-whatsapp")}
+              icon={<WhatsappIcon className="size-4" />}
+            />
+            <SocialSettingInput
+              settingKey="social-discord"
+              settingValue={getSettingValue("social-discord")}
+              icon={<DiscordIcon className="size-4" />}
+            />
+            <SocialSettingInput
+              settingKey="social-github"
+              settingValue={getSettingValue("social-github")}
+              icon={<GithubIcon className="size-4" />}
+            />
+            <SocialSettingInput
+              settingKey="social-instagram"
+              settingValue={getSettingValue("social-instagram")}
+              icon={<InstagramIcon className="size-4" />}
+            />
+            <SocialSettingInput
+              settingKey="social-matrix"
+              settingValue={getSettingValue("social-matrix")}
+              icon={<MatrixIcon className="size-4" />}
+            />
+          </CardContent>
+        </Card>
+      </section>
+    )
+  );
+}
+
+interface SocialSettingInputProps {
+  settingKey: string;
+  settingValue: string;
+  icon: React.ReactNode;
+  placeholder?: string;
+}
+
+function SocialSettingInput({
+  icon,
+  settingValue,
+  settingKey,
+  placeholder = "Link",
+}: SocialSettingInputProps) {
+  const { sid } = useUser();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      input: settingValue,
+    },
+  });
+
+  async function changeSetting(data: z.infer<typeof FormSchema>) {
+    const client = getClient(sid!);
+    try {
+      client.request<UpsertSettingMutation>(UpsertSettingDocument, {
+        setting: {
+          key: settingKey,
+          value: data.input,
+          type: ScalarType.String,
+        },
+      });
+      toast.info("Erfolgreich gesichert!");
+    } catch {
+      toast.error(
+        "Einstellung konnte nicht gespeichert werden. Bitte versuche es erneut."
+      );
+    }
+  }
+
+  return (
+    <div className="flex flex-row items-center gap-x-4 w-full">
+      {icon}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(changeSetting)}
+          className="flex flex-row gap-x-2 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="input"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder={placeholder} {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" variant="secondary">
+            <Save className="size-4" />
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
