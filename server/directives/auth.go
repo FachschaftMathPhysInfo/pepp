48121a9f0359ce7ad3d2c7e4e9f83f2857c55a2c
Daniel Heidemann <mail@dheidemann.de>
2025-09-30 13:12:06 +0200

    Implement: IS_TUTOR directive

--- server/directives/auth.go
+++ server/directives/auth.go
@@ -8,14 +8,15 @@ import (
 	"github.com/FachschaftMathPhysInfo/pepp/server/graph/model"
 	"github.com/FachschaftMathPhysInfo/pepp/server/models"
 	log "github.com/sirupsen/logrus"
+	"github.com/uptrace/bun"
 )
 
-func Auth(ctx context.Context, obj interface{}, next graphql.Resolver, rule *model.Rule, role *model.Role) (res interface{}, err error) {
+func Auth(ctx context.Context, obj interface{}, next graphql.Resolver, rule *model.Rule, role *model.Role, db *bun.DB, env string) (res interface{}, err error) {
 	err = fmt.Errorf("Access denied")
 
 	authenticatedUser, _ := ctx.Value("user").(*models.User)
 
-	if model.Role(authenticatedUser.Role) == model.RoleAdmin {
+	if model.Role(authenticatedUser.Role) == model.RoleAdmin || env != "Production" {
 		return next(ctx)
 	}
 
@@ -40,14 +41,29 @@ func Auth(ctx context.Context, obj interface{}, next graphql.Resolver, rule *mod
 		if ids, ok := args["userID"].([]int); ok && len(ids) == 1 {
 			id = ids[0]
 		}
-		log.Info(id)
-		log.Info(authenticatedUser.ID)
 
 		if mail != authenticatedUser.Mail && mail != authenticatedUser.SessionID && id != int(authenticatedUser.ID) {
 			log.Info("Access denied for ", authenticatedUser.Mail)
 			return nil, err
 		}
-		// TODO: model.RuleIsTutor
+	case model.RuleIsTutor:
+		tut, ok := obj.(*models.Tutorial)
+		if !ok {
+			return nil, fmt.Errorf("invalid object for directive")
+		}
+
+		exists, err := db.NewSelect().
+			Model((*models.TutorialToUserAssignment)(nil)).
+			Where("user_id = ?", authenticatedUser.ID).
+			Where("tutorial_id = ?", tut.ID).
+			Exists(ctx)
+		if err != nil {
+			log.Error(err)
+			return nil, fmt.Errorf("error while checking for tutor to tutorial relation")
+		}
+		if !exists {
+			return nil, fmt.Errorf("user is not permitted to query for this tutorials students")
+		}
 	}
 
 	return next(ctx)
