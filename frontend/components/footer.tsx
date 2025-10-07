9f3602d86633ab5cfad7ce4170c1d1ec94ae7e64
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-10-07 21:17:43 +0200

    prettify: footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,6 +1,10 @@
+import {Bug, Github} from "lucide-react";
+import Link from "next/link";
+
 export function Footer() {
   return (
-    <footer className="bottom-0 left-0 w-full py-5 px-10 text-sm text-muted-foreground border-t mt-5">
+    <footer
+      className="bottom-0 left-0 w-full py-5 px-10 text-sm text-muted-foreground border-t mt-5 flex items-center justify-between">
       <span>
         Built by{" "}
         <a className="cursor-pointer underline" href="https://mathphys.info">
@@ -8,15 +12,14 @@ export function Footer() {
         </a>{" "}
         at Heidelberg University.{" "}
       </span>
-      <span>
-        The source code is available on{" "}
-        <a
-          className="cursor-pointer underline"
-          href="https://github.com/FachschaftMathPhysInfo/pepp"
-        >
-          GitHub
-        </a>
-        .
+
+      <span className={'flex gap-x-4 items-center'}>
+        <Link href={'https://github.com/FachschaftMathPhysInfo/pepp'}>
+          <Github className={'stroke-muted-foreground'}/>
+        </Link>
+        <Link href={'https://github.com/FachschaftMathPhysInfo/pepp/issues/new?template=bug_report.md'}>
+          <Bug className={'stroke-muted-foreground'}/>
+        </Link>
       </span>
     </footer>
   );
