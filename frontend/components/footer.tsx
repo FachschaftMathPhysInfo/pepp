dd4a24bee7f35110dfae7c91de4ff6538b3cc1c4
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-10-07 21:24:27 +0200

    fix: use official image cuz I don't wanna be sued by microsoft

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,7 +1,13 @@
-import {Bug, Github} from "lucide-react";
+"use client"
+
+import {Bug} from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
+import {useTheme} from "next-themes";
 
 export function Footer() {
+  const {resolvedTheme} = useTheme()
+
   return (
     <footer
       className="bottom-0 left-0 w-full py-5 px-10 text-sm text-muted-foreground border-t mt-5 flex items-center justify-between">
@@ -15,7 +21,13 @@ export function Footer() {
 
       <span className={'flex gap-x-4 items-center'}>
         <Link href={'https://github.com/FachschaftMathPhysInfo/pepp'}>
-          <Github className={'stroke-muted-foreground'}/>
+          <Image
+            suppressHydrationWarning
+            src={resolvedTheme === "light" ? "/github_light.svg" : "/github_dark.svg"}
+            alt={'GitHub Logo'}
+            width={24}
+            height={24}
+          />
         </Link>
         <Link href={'https://github.com/FachschaftMathPhysInfo/pepp/issues/new?template=bug_report.md'}>
           <Bug className={'stroke-muted-foreground'}/>
